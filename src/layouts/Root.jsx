5958988199ef7cc2896ccb85962704daf14c12e5
Sanjay <sanzaikc@gmail.com>
2022-11-14 21:47:04 +0545

    mend

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -6,7 +6,7 @@ const Root = () => {
   return (
     <div className="h-screen w-screen bg-sky-200">
       <div className="container relative mx-auto flex h-full max-w-md flex-col bg-white shadow-lg">
-        <div className="flex-grow overflow-scroll px-4 shadow-inner">
+        <div className="flex-grow bg-red-200 px-4 shadow-inner">
           <Outlet />
         </div>
 
--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -2,7 +2,6 @@ import { useRouteError } from "react-router-dom";
 
 const NotFound = () => {
   const error = useRouteError();
-  console.error(error);
 
   return (
     <div>
