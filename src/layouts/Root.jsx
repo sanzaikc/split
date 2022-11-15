03aca84b920f8085142790fa1dc0358cfd2d5986
Sanjay <sanzaikc@gmail.com>
2022-11-15 08:06:02 +0545

    mend

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -4,7 +4,7 @@ import { BottomNavigation } from "../components/BottomNavigation";
 
 const Root = () => {
   return (
-    <div className="relative h-screen min-h-screen w-screen  bg-blue-200 tracking-wide dark:bg-slate-800 dark:text-gray-200">
+    <div className="relative h-screen min-h-screen w-screen  bg-blue-200 tracking-wide text-slate-700 dark:bg-slate-800 dark:text-gray-400">
       <div className="container relative mx-auto flex h-full max-w-md flex-col shadow-lg">
         <div className="flex-grow bg-transparent px-4 shadow-inner">
           <Outlet />
