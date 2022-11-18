54ad7906c88e4d8c576fe49ebfbeba368410180c
Sanjay <sanzaikc@gmail.com>
2022-11-18 16:59:51 +0545

    mend

--- src/components/shared/WithHeader.tsx
+++ src/components/shared/WithHeader.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import { ArrowLeftIcon } from "assets/svg";
+import ThemeContainer from "./ThemeContainer";
 
 type WithHeaderProps = {
   children: React.ReactNode;
@@ -19,19 +20,21 @@ const WithHeader = ({
 
   return (
     <div className="flex h-full flex-col">
-      <div
-        className=" flex flex-shrink-0 items-center space-x-4 p-4  text-lg font-semibold"
-        style={{ boxShadow: "0 3px 5px -5px currentcolor" }}
-      >
-        {!disableBack && (
-          <ArrowLeftIcon
-            className="h-5 font-bold"
-            onClick={() => navigate(-1)}
-          />
-        )}
+      <ThemeContainer>
+        <div
+          className="flex flex-shrink-0 items-center space-x-4 p-4  text-lg font-semibold"
+          style={{ boxShadow: "0 3px 5px -5px currentcolor" }}
+        >
+          {!disableBack && (
+            <ArrowLeftIcon
+              className="h-5 font-bold"
+              onClick={() => navigate(-1)}
+            />
+          )}
 
-        <div className="tracking-wider">{title} </div>
-      </div>
+          <div className="tracking-wider">{title} </div>
+        </div>
+      </ThemeContainer>
 
       <div className="flex-grow px-4 py-2">{children}</div>
     </div>
