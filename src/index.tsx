a0f022b54729891148ca93f0dd71bdc8348488e8
Sanjay <sanzaikc@gmail.com>
2022-11-23 08:40:20 +0545

    mend

--- src/index.tsx
+++ src/index.tsx
@@ -15,9 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      {/* <DarkThemeProvider> */}
-      <App />
-      {/* </DarkThemeProvider> */}
+      <DarkThemeProvider>
+        <App />
+      </DarkThemeProvider>
     </MantineProvider>
   </React.StrictMode>
 );
